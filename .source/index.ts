4e99c2eb9db94f278b048228f7bf6451b63bc8e0
Sai Lakshmi <sailakshmipriyamarturu@gmail.com>
2025-10-30 18:20:50 +0530

    first commit

---
+++ .source/index.ts
@@ -0,0 +1,6 @@
+// @ts-nocheck -- skip type checking
+import * as docs_1 from "../content/docs/blind75/two-sum.mdx?collection=docs"
+import * as docs_0 from "../content/docs/intro.mdx?collection=docs"
+import { _runtime } from "fumadocs-mdx/runtime/next"
+import * as _source from "../source.config"
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"intro.mdx","fullPath":"content\\docs\\intro.mdx"}, data: docs_0 }, { info: {"path":"blind75/two-sum.mdx","fullPath":"content\\docs\\blind75\\two-sum.mdx"}, data: docs_1 }], [])
