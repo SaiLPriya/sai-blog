7d11012e8bbfe5ef6974f186920dc74722bc47a9
Sai Lakshmi <sailakshmipriyamarturu@gmail.com>
2025-11-06 12:00:37 +0530

    Duplicates in array md file added

--- .source/index.ts
+++ .source/index.ts
@@ -1,6 +1,7 @@
 // @ts-nocheck -- skip type checking
-import * as docs_1 from "../content/docs/blind75/Balanced-Brackets.mdx?collection=docs"
+import * as docs_2 from "../content/docs/DSA/Duplicates-in-Array.mdx?collection=docs"
+import * as docs_1 from "../content/docs/DSA/Balanced-Brackets.mdx?collection=docs"
 import * as docs_0 from "../content/docs/intro.mdx?collection=docs"
 import { _runtime } from "fumadocs-mdx/runtime/next"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"intro.mdx","fullPath":"content\\docs\\intro.mdx"}, data: docs_0 }, { info: {"path":"blind75/Balanced-Brackets.mdx","fullPath":"content\\docs\\blind75\\Balanced-Brackets.mdx"}, data: docs_1 }], [])
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"intro.mdx","fullPath":"content\\docs\\intro.mdx"}, data: docs_0 }, { info: {"path":"DSA/Balanced-Brackets.mdx","fullPath":"content\\docs\\DSA\\Balanced-Brackets.mdx"}, data: docs_1 }, { info: {"path":"DSA/Duplicates-in-Array.mdx","fullPath":"content\\docs\\DSA\\Duplicates-in-Array.mdx"}, data: docs_2 }], [])
